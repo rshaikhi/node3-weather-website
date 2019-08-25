af51b82ffe33f06666be100f0dac0a698fced0ab
COMPUTER\ramin <raminshaikhi@gmail.com>
2019-08-24 20:35:41 -0400

    Updated the forecast string to include new data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lat, long, callback) => {
         } else if (body.error) {
             callback('Unable to find the location', undefined);
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out.' + ' There is a ' + body.currently.precipProbability + ' chance of rain.');
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out (High of: ' + body.daily.data[0].temperatureHigh + ', Low of: ' + body.daily.data[0].temperatureLow + '). There is a ' + body.currently.precipProbability + ' chance of rain.');
         }
     })
 
